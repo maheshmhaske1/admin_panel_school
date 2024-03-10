1673e80edadc11e90f2196f29f7f4e09cb0aef32
satyajit1712 <satyajit.v@project42labs.com>
2024-03-10 21:43:07 +0530

    changes in base url

--- src/utility/api.js
+++ src/utility/api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import Cookies from 'js-cookie'
 
 // const BASE_URL = 'http://localhost:5000'
-const BASE_URL = 'http://66.179.250.128:5000'
+const BASE_URL = 'https://leadplanner.lotusx.shop/api/'
 
 
 export const axiosInstance = axios.create({
